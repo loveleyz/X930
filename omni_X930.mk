f2473f1c5b4a0b72e66e7ddb589096c86f9f1aae
loveleyz <loveleyz14@gmail.com>
2020-08-12 19:33:38 +0800

    fisrt commit

---
+++ omni_X930.mk
@@ -0,0 +1,5 @@
+$(call inherit-product, vendor/omni/config/common.mk)
+
+$(call inherit-product, device/Cherry_Mobile/X930/full_X930.mk)
+
+PRODUCT_NAME := omni_X930
---
+++ vendorsetup.sh
@@ -0,0 +1,2 @@
+add_lunch_combo omni_X930-eng
+add_lunch_combo omni_X930-userdebug
