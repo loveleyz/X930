19770f4c08c50069bde57726750e607c9d7bcc8c
loveleyz <loveleyz14@gmail.com>
2020-08-13 19:57:50 +0800

    delete all

--- Android.mk
+++
@@ -1,6 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-ifneq ($(TARGET_DEVICE),Flare_S8)
-include $(call all-makefiles-under,$(LOCAL_PATH))
-include $(CLEAR_VARS)
-endif
--- AndroidProducts.mk
+++
@@ -1,2 +0,0 @@
-PRODUCT_MAKEFILES := \
-	$(LOCAL_DIR)/omni_Flare_S8.mk
