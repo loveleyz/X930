fc3b11378793ff73cfff62da7af4093e932d8fe7
loveleyz <loveleyz14@gmail.com>
2020-08-14 08:37:05 +0800

    Eperimetnal

---
+++ omni_X930.mk
@@ -0,0 +1,15 @@
+$(call inherit-product, build/target/product/embedded.mk)
+
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/omni/config/common.mk)
+
+PRODUCT_DEVICE := Flare_S8
+PRODUCT_NAME := omni_X930
+PRODUCT_BRAND := Cherry_Mobile
+PRODUCT_MODEL := Flare_S8
+PRODUCT_MANUFACTURER := Cherry_Mobile
+
+# ADB Fix
+PRODUCT_PROPERTY_OVERRIDES += \
+    sys.usb.ffs.aio_compat=true
+
