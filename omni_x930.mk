b2076b7b2c28149aca1eadef91a32f786d08cb71
loveleyz <loveleyz14@gmail.com>
2020-09-15 22:02:47 +0800

    Add files via upload

--- omni_x930.mk
+++ omni_x930.mk
@@ -1,5 +1,5 @@
 # Release name
-PRODUCT_RELEASE_NAME := X930
+PRODUCT_RELEASE_NAME := x930
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
@@ -9,7 +9,7 @@ $(call inherit-product, vendor/omni/config/common.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_BRAND := Cherry_Mobile
-PRODUCT_DEVICE := X930
+PRODUCT_DEVICE := x930
 PRODUCT_MANUFACTURER := Cherry_Mobile
 PRODUCT_MODEL := Flare_S8
 PRODUCT_NAME := omni_x930
