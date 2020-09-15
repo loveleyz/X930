8358a5a360af2a6e2c0c90f20244ad2e979a62a3
loveleyz <loveleyz14@gmail.com>
2020-09-15 22:20:15 +0800

    Update omni_x930.mk

--- omni_x930.mk
+++ omni_x930.mk
@@ -5,7 +5,7 @@ PRODUCT_RELEASE_NAME := x930
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from our custom product configuration
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/pb/config/common.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_BRAND := Cherry_Mobile
